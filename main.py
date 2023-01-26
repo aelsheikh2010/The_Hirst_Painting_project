80983f9cfed0f91ebf601e42b651a40514c2869f
Ahmed Elsheikh <golf17@gmail.com>
2023-01-27 00:02:14 +0200

    Initial commit

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 # colors = colorgram.extract('image.jpg', 30)
 # print(colors)
 # new_colors = []
-import turtle
 #import turtle
-
+#import turtle
+import turtle
 # for color in colors:
 #     r = color.rgb.r
 #     g = color.rgb.g
@@ -20,9 +20,6 @@ import turtle
 #     b = rgb.b
 #     new_colors.append((r, g, b))
 # print(new_colors)
-
-
-#color_list = [(192, 165, 115), (138, 166, 190), (56, 102, 140), (141, 91, 50), (12, 23, 55), (222, 207, 123), (182, 154, 42), (61, 22, 11), (182, 146, 165), (142, 177, 151), (72, 117, 81), (58, 15, 26), (126, 79, 102), (130, 30, 16), (15, 39, 23), (24, 53, 127), (177, 188, 215), (164, 104, 134), (115, 31, 46), (97, 150, 100), (98, 121, 172), (210, 178, 197), (174, 105, 93), (74, 150, 165), (25, 91, 65), (172, 205, 180)]
 
 from turtle import Turtle, Screen
 import random
